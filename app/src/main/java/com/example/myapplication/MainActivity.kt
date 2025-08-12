f2bbf3d8106bcef8744a4cb0ac26e7e0bf0fd016
Mohamed Seedyk <mohamedseedk908@gmail.com>
2025-08-12 15:23:52 +0300

    finish view result

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){  // : دي معناها اكستند يعني وراثة
     override fun onCreate(savedInstanceState: Bundle?) {
+        val TAG = "MainActivity"
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val converterButton : Button = findViewById(R.id.convert_button)
+        val amountEditText : TextInputEditText = findViewById(R.id.amount_edit_text)
+        val resultView : TextInputEditText = findViewById(R.id.result_view)
+        val text = "Hello toast!"
+        val duration = Toast.LENGTH_SHORT
+        converterButton.setOnClickListener {
+            val amount = amountEditText.text.toString().toDouble()
+            val result = (48.39) * amount
+            resultView.setText(result.toString())
         }
+
     }
+
 }
+
+
